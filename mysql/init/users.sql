ea09df3dafe63705eff70e5379805323b87a7950
Sakamoto0525 <525masaya@gmail.com>
2021-04-13 09:45:25 +0900

    bookのCARD機能作成

---
+++ mysql/init/users.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+    id integer,
+    name varchar(255),
+    age integer
+);
+
+INSERT INTO users VALUES
+(1, 'Alice', 20),
+(2, 'Bob', 20),
+(3, 'Carol', 20)
